1a30935609850548d47d1197fedb3768cdec03d1
Mansour Behabadi <mbehabadi@mathspace.com.au>
2023-03-29 02:06:33 +1100

    Make an actual working autorelaod

--- autoreload.sh
+++ autoreload.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-air --build.cmd "go build -o /tmp/cgp *.go" --build.bin "/tmp/cgp"
+while true; do
+  echo "Restarting ..."
+  while ! go build -o /tmp/cgpt; do sleep 1; done
+  /tmp/cgpt &
+  inotifywait . -r -e modify -e move -e delete -e create || true
+  kill -9 $(jobs -p) || true &>/dev/null
+done
