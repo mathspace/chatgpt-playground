b70d5a2b1a284167ddb0f7e07df94be9bb2f952e
Mansour Behabadi <mbehabadi@mathspace.com.au>
2025-05-21 20:36:14 +1000

    Do not close modal when dragging to outside starting from inside

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -2,12 +2,13 @@ import "./Modal.css";
 import { useCallback } from "react";
 
 export function Modal({ onCancel, width, children, contentClassName }) {
-  const stopProp = useCallback(e => e.stopPropagation());
-  return <div onClick={onCancel} className="modal">
+  const stopProp = useCallback(e => e.stopPropagation(), []);
+  return <div onMouseDown={onCancel} className="modal">
     <div
+      onMouseDown={stopProp}
       onClick={stopProp}
       className={`content ${contentClassName}`}
       style={{ width: width }}
     >{children}</div>
-  </div >;
+  </div>;
 }
